6edeff7343a4a3667fb96a41258d9f6e2998b37c
weiyikai <weiyikai@DESKTOP-AR8RGD4>
2018-10-19 00:43:00 +0800

    增加了MyMap的put,get方法

--- Collection/src/com/wyklovelf/map105/MyMap.java
+++ Collection/src/com/wyklovelf/map105/MyMap.java
@@ -8,8 +8,34 @@ package com.wyklovelf.map105;
  *
  */
 public class MyMap {
-	public static void main(String[] args) {
+	
+	int size;//Map中条目(MyEntry的数量)
+	
+	MyEntry []arr = new MyEntry[999];
+	
+	public void put(Object key,Object value) {
+		MyEntry e = new MyEntry(key,value);
 		
+		arr[size++] = e;
+	}
+	
+	public Object get(Object key) {
+		for (int i = 0; i < this.size; i++) {
+			if(arr[i].key.equals(key)) {
+				return arr[i].value;
+			}
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		MyMap m = new MyMap();
+		
+		m.put("高琪", new Wife("杨幂"));
+		
+		Wife w = (Wife)m.get("高琪");
+		
+		System.out.println(w.name);
 	}
 	
 	/**
@@ -26,4 +52,10 @@ class MyEntry{
 	Object key;
 	
 	Object value;
+
+	public MyEntry(Object key, Object value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
 }
