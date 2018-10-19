810b1d9dae38678fcf00aaebd7b9df4ae2670254
weiyikai <weiyikai@DESKTOP-AR8RGD4>
2018-10-20 00:47:53 +0800

    增加MyMap的containsKey 和  containsValue方法

--- Collection/src/com/wyklovelf/map105/MyMap.java
+++ Collection/src/com/wyklovelf/map105/MyMap.java
@@ -4,6 +4,7 @@ package com.wyklovelf.map105;
 /**
  * 前面使用了JDK里面的Map，现在自己不完美地实现以下Map
  * Map:存放键值对，根据键对象找对应的值对象
+ * 缺点：效率低
  * @author weiyikai
  *
  */
@@ -15,6 +16,14 @@ public class MyMap {
 	
 	public void put(Object key,Object value) {
 		MyEntry e = new MyEntry(key,value);
+		
+		//键不能重复，否则后一个会把前一个给覆盖掉 
+		for (int i = 0; i < this.size; i++) {
+			if(arr[i].key.equals(key)) {
+				arr[i].value = value;
+				return;
+			}
+		}
 		
 		arr[size++] = e;
 	}
@@ -28,6 +37,24 @@ public class MyMap {
 		return null;
 	}
 	
+	public boolean containsKey(Object key) {
+		for (int i = 0; i < this.size; i++) {
+			if(arr[i].key.equals(key)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean containsValue(Object value) {
+		for (int i = 0; i < this.size; i++) {
+			if(arr[i].value.equals(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		MyMap m = new MyMap();
 		
@@ -36,6 +63,8 @@ public class MyMap {
 		Wife w = (Wife)m.get("高琪");
 		
 		System.out.println(w.name);
+		
+		System.out.println();
 	}
 	
 	/**
---
+++ Collection/src/com/wyklovelf/map105/MyMap02.java
@@ -0,0 +1,11 @@
+package com.wyklovelf.map105;
+
+
+/**
+ * 提高了查询效率的MyMap
+ * @author weiyikai
+ *
+ */
+public class MyMap02 {
+	
+}
